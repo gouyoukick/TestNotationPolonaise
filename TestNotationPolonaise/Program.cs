889a5dd0d27c2bfbb622f8185ccb170dd5d7b875
gouyoukick <gaulierjulien@yahoo.fr>
2021-10-18 22:09:15 +0200

    Programme de calcul avec une notation polonaise opérationnel enfin

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -10,6 +10,84 @@ namespace TestNotationPolonaise
     class Program
     {
         /// <summary>
+        /// Procédure de décalage des éléments d'un tableau de 2 cases vers la gauche.
+        /// </summary>
+        /// <param name="tab">tableau à décaler</param>
+        /// <param name="position">position de début de décalage</param>
+        static void Decalage(string[] tab, int position )
+        {
+            for (int l = position; l < (tab.Length - 3); l++)
+            {
+                tab[l + 1] = tab[l + 3];
+            }
+            tab[tab.Length - 5] = "";
+            tab[tab.Length - 4] = "";
+        }
+        
+        /// <summary>
+        /// Calcul avec formulation Polonaise pour les opérations +, -, / et *
+        /// </summary>
+        /// <param name="formule">la formule Polonaise à calculer</param>
+        /// <returns></returns>
+        static string Polonaise(String formule)
+        {
+            //detection erreur de saisie
+            try
+            {
+                //découpage de la formule et remplissage du tableau
+                string[] tab = (formule + "   ").Split(' ');
+                //Lecture du tableau en partant de la fin
+                for (int k = (tab.Length - 4); k > -1; k--)
+                {
+                    switch (tab[k])
+                    {
+                        //addition
+                        case "+":
+                            tab[k] = (float.Parse(tab[k + 1]) + float.Parse(tab[k + 2])).ToString();
+                            Decalage(tab, k);
+                            break;
+                        //soustraction
+                        case "-":
+                            tab[k] = (float.Parse(tab[k + 1]) - float.Parse(tab[k + 2])).ToString();
+                            Decalage(tab, k);
+                            break;
+                        //Multiplication
+                        case "*":
+                            tab[k] = (float.Parse(tab[k + 1]) * float.Parse(tab[k + 2])).ToString();
+                            Decalage(tab, k);
+                            break;
+                        //Division
+                        case "/":
+                            if(float.Parse(tab[k + 2]) != 0)
+                            {
+                                tab[k] = (float.Parse(tab[k + 1]) / float.Parse(tab[k + 2])).ToString();
+                                Decalage(tab, k);
+                            }
+                            //Arret si Division par O
+                            else
+                            {
+                                return "NaN";
+                            }
+                            break;
+                    }
+                }
+                //Arret si tous les chiffres ne sont pas consommés
+                if (tab[1] != "")
+                {
+                    return "NaN";
+                }
+                else
+                {
+                    return tab[0];
+                }
+            }
+            catch
+            {
+                return "NaN";
+            }
+
+        }
+        /// <summary>
         /// saisie d'une réponse d'un caractère parmi 2
         /// </summary>
         /// <param name="message">message à afficher</param>
